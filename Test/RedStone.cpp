9a91e151cf77b00d95357cb421ed115cd664001b
刘鹏飞 <liupengfei@fuyuqingdeMBP.lan>
2017-04-21 01:04:47 +0800

    remove all

--- Test/RedStone.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "RedStone.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RedStone, "RedStone" );
-
-DEFINE_LOG_CATEGORY(LogRedStone)
- 
